99d5da464637c83bfd9a24e95ee46ad717eb7490
汪凯 <2538803840@qq.com>
2021-01-31 19:22:37 +0800

    2.0

--- Poker/Main.java
+++ Poker/Main.java
@@ -16,11 +16,29 @@ public class Main {
     }
 
     private static void Play(List<Card> list, int n) {
-        for(int i=0;i<n;i++){
-            for(int j=0;j<5;j++){
-                System.out.print(list.remove(j));
+        // 3. 发牌, 假设有三个玩家, 每个玩家, 给发 5 张牌
+// 每个玩家手里有 5 张牌, 这 5 张牌就使用另外一个 ArrayList 表示.
+//        List<Card> player1 = new ArrayList<>();
+//        List<Card> player2 = new ArrayList<>();
+//        List<Card> player3 = new ArrayList<>();
+        // players 类型仍然是 List, 泛型参数, 是一个 List<Card> , 每个元素就是一个 List<Card>
+        List<List<Card>> players = new ArrayList<>();
+        // 每次 add 的元素都是一个 ArrayList<Card> 类型
+        players.add(new ArrayList<>());
+        players.add(new ArrayList<>());
+        players.add(new ArrayList<>());
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                Card top = list.remove(0);
+                List<Card> player = players.get(i);
+                player.add(top);
             }
-            System.out.println();
+        }
+
+        // 4. 展示手牌
+        for (int i = 0; i < players.size(); i++) {
+            List<Card> player = players.get(i);
+            System.out.println("玩家" + i + " 的手牌是: " + player);
         }
     }
 
